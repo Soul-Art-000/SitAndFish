ae18dafbc6af27b4277ec491ba78c8c7c8521f6a
S <salihbayrak815@gmail.com>
2026-02-26 14:11:34 +0300

    Fix: Add StopAnimation and completelyStopAnimatingOrDoingAction to perfectly clear stuck state

--- SitAndFish/ModEntry.cs
+++ SitAndFish/ModEntry.cs
@@ -128,7 +128,12 @@ namespace SitAndFish
                 farmer.yOffset = 0f; // offset'i sıfırla
                 
                 // Animasyon kilidinden dolayı karakterin buga girmemesi için 
-                // balık tutma bittiğinde oturmayı tam olarak sonlandırıyoruz.
+                // zorlanmış animasyonu ve eylemi tamamen durdur.
+                farmer.FarmerSprite.StopAnimation();
+                farmer.completelyStopAnimatingOrDoingAction();
+                farmer.Halt();
+
+                // Eğer hala oturuyor görünüyorsa, kalkmasını sağla
                 if (farmer.isSitting.Value)
                 {
                     farmer.StopSitting(true);
